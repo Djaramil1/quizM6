984e6ba7a60ffcdac257d95e80a1d8170a110754
Djaramil1 <dyrce.jaramillo@gmail.com>
2015-07-28 22:40:55 -0300

    tarea Mod 6

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var quizController = require('../controllers/quiz_controller');
+
+var quizController = require('../controllers/quiz-controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 module.exports = router;
